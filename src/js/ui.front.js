7423c9afc368f44c2f3688b4ff39928e6e8f0d66
playmoreh2 <playmore@nate.com>
2023-01-31 14:32:45 +0900

    gg

---
+++ src/js/ui.front.js
@@ -0,0 +1,12 @@
+(function(t){
+	var ui = {
+		init : function(){
+			ui.tabInit();//
+			ui.iptInit();//
+			ui.accoInit();//
+			ui.tblInit();//
+			ui.swiperInit();//
+			tip.init();//
+		}
+	}
+})()
