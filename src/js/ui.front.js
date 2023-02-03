758eac6a1143db709ed89e0b68ab8a148c840c48
playmoreh2 <playmore@nate.com>
2023-02-03 11:03:54 +0900

    hh

--- src/js/ui.front.js
+++ src/js/ui.front.js
@@ -7,6 +7,24 @@
 			ui.tblInit();//
 			ui.swiperInit();//
 			tip.init();//
+			ui.progressInit();
+		},
+		progressInit : function(){
+			console.log("progressInit");
+			for(var  i = 0 ; i < $('.progress').length ; ++i ){
+				if( $('.progress').eq(i).hasClass('uiAct') == false ){
+					$('.progress').eq(i).addClass('uiAct');
+					$('.progress:eq('+i+') ol').attr('aria-hidden','true');
+					var num = $('.progress').eq(i).find('li').length;
+					var nowStep = $('.progress').eq(i).find('li.on').index() + 1;
+					var stepName = $('.progress:eq('+i+') li.on').text();
+					$('.progress:eq('+i+')').attr('role','img');
+					$('.progress:eq('+i+')').attr('aria-label','총 '+num+'단계 중 '+nowStep+'단계 '+stepName + ' 진행중');
+					if( $('.progress:eq('+i+') li.on span').length == 0 ){
+						$('.progress:eq('+i+') li.on').wrapInner('<span/>');
+					}
+				}
+			}
 		}
 	}
 })()
